1d48aa9dd8c102b8725e13e52a3faf8898c868af
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-01-07 14:29:27 +0800

    fix: _app.js error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
+import {Inter} from "@next/font/google"
+
+const inter = Inter({subsets: ["latin"]})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <div className={inter.className + "h-screen min-h-screen bg-white"}>
+     <Component {...pageProps} />
+     </div>
+)}
