5456af3e0db857d5d1f8effe9e8729757706effc
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-01-16 20:30:59 +0800

    chore: customize Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import {Inter} from "@next/font/google"
+import { Layout } from '../components/Layout'
 
 const inter = Inter({subsets: ["latin"]})
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={inter.className + "h-screen min-h-screen bg-white"}>
-     <Component {...pageProps} />
-     </div>
+    // <div className={inter.className + "h-screen min-h-screen bg-white"}>
+     <Layout>
+      <Component {...pageProps} />
+     </Layout>
+    //  </div>
 )}
