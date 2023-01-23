82a68e7b4b952ce030d780043d4c4e07b31bf220
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-01-23 13:41:12 +0000

    chore: search page + pdf test

--- data/research.js
+++ data/research.js
@@ -1,11 +1,6 @@
-import Researches from '../public/pdfs/A Quantitative Study about Youtube College Vlogs.pdf'
-
-import React from 'react'
-
-const research = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default research
+const pdfs = [{
+  title:"React",
+  author:"12 STEM-1",
+  schoolYear:"2022-2023",
+  file:"/pdfs/A Quantitative Study about Youtube College Vlogs.pdf"
+}]
