df25314681b6370e18c666f9ef0018d9fcdee8a7
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-01-28 22:36:33 +0800

    try svg as component

---
+++ components/svg/aboutFiles.jsx
@@ -0,0 +1,13 @@
+import { ReactComponent as Files } from "../../public/files-about.svg";
+
+import React from 'react'
+
+const aboutFiles = () => {
+  return (
+    <div>
+      <Files/>
+    </div>
+  )
+}
+
+export default aboutFiles
