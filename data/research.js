5a682a5a410b7f25f9b315c05a49a69ffb8a11ae
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-01-28 22:34:44 +0800

    Delete research.js

--- data/research.js
+++
@@ -1,6 +0,0 @@
-const pdfs = [{
-  title:"A Quantitative Study about Youtube College Vlogs.pdf",
-  author:"12 STEM-1",
-  schoolYear:"2022-2023",
-  file:"/pdfs/A Quantitative Study about Youtube College Vlogs.pdf"
-}]
