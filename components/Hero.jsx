fef6f477dd28a756abca8a706e333bb0a83a2e14
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-01-30 16:32:50 +0800

    chore: add padding top

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ import Search from '../pages/Search'
 
 const Hero = () => {
   return (
-        <section class="bg-white dark:bg-gray-900 mt-4 pt-12" id='home'>
+        <section class="bg-white dark:bg-gray-900 mt-4 pt-20" id='home'>
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <a href="#about" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
