373eacf2d81b329f12397b8495905a4276490922
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-04 20:38:22 +0800

    chore: update typography

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,9 +15,8 @@ const Footer = () => {
                     height={50}
                     /> */}
                     <h1 className='text-xl font-extrabold tracking-tight leading-none text-gray-800 md:text-xl lg:text-2xl dark:text-white'>
-                    Pathfinder
+                    PATHFINDER
                     </h1>
-                    <p></p>
                 </a>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
