a06066e69cca9a683b4c39e7018dfca10c14dcc9
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-04 20:35:43 +0800

    chore: add Head component to article.js page

--- pages/articles.js
+++ pages/articles.js
@@ -1,12 +1,23 @@
-import SearchBar from '../components/search-articles/SearchBar'
+import Head from 'next/head';
 import ResearchCard from '../components/search-articles/ResearchCard'
 
 const Search = () => {
   return (
-    <div>
-      <SearchBar/> 
-      <ResearchCard/>
-    </div>
+    <>
+      <Head>
+        <title>Pathfinder | Articles</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/pucu.png" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/flowbite.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/flowbite.min.js" defer/>
+        <script src="../path/to/flowbite/dist/flowbite.min.js" defer/>
+      </Head>
+     <main className='contain min-h-screen'>
+           <ResearchCard/>
+      </main>
+    </>
+  
   )
 }
 
