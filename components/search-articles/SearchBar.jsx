b388ea423562eb23a39b92c3cf9bd83a4fbb61ca
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-05 17:24:05 +0800

    fix: cards size

--- components/search-articles/SearchBar.jsx
+++ components/search-articles/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import data from '../../data/research.json'
+
 
 
 const SearchBar = ({ data, onSearch }) => {
@@ -11,29 +12,30 @@ const SearchBar = ({ data, onSearch }) => {
     }
 
   return (
+    <>
       <div>
-       
-      <form class="flex justify-center items-center ">   
-          <label for="simple-search" class="sr-only">Search</label>
-          <div class="relative w-5/6">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-              </div>
+      <form class="flex justify-center items-center">   
+          <label for="simple-search" class="sr-only">Search related studies</label>
+          <div class="w-4/5">
+          {/* <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        </div> */}
               <input type="text" 
                     onChange={handleSearch}
                     value={searchTerm}
                     id="simple-search" 
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500" 
-                    placeholder="Search" 
+                    class="h-14 bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-md focus:ring-teal-500 focus:border-teal-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500" 
+                    placeholder="Search related studies" 
                     required
                 />
           </div>
-          <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-teal-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-              <span class="sr-only">Search</span>
+          <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-teal-700 rounded-md border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              <span class="sr-only">Search related studies</span>
           </button>
       </form>
     </div>
+    </>
   )
 }
 
