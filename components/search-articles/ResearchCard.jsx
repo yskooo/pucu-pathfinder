fb4f8b8b9a5ae1f88332ce8c0f4fd10a215201fc
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-05 17:23:49 +0800

    chore: refractor

--- components/search-articles/ResearchCard.jsx
+++ components/search-articles/ResearchCard.jsx
@@ -15,39 +15,10 @@ const ResearchCard = () => {
     setSearchResults(filteredData);
   }
 
-//   return (
-//     <div>
-//       <SearchBar data={data} onSearch={handleSearch} />
-//       <div className='grid grid-cols-3 gap-4 m-6'>
-//         {searchResults.map(item => (
-//           <div key={item.id}>
-//             {/* {data.map(item =>  */}
-//               <Link href={(item.file)} key={item.id} class="flex flex-col items-enter bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" target="_blank" rel='noreferrer'>
-//                 <Image
-//                   className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" 
-//                   src={(item.image)}
-//                   alt={item.title}
-//                   width={200}
-//                   height={200}
-//               />
-//                 <div class="flex flex-col justify-between p-4 leading-normal">
-//                     <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{(item.title)}</h5>
-//                     <p className="font-medium text-gray-700 dark:text-gray-400">Researchers: {(item.author)}</p>
-//                     <p className="font-normal text-gray-700 dark:text-gray-400">Category: {(item.category)}</p>
-//                     <p className="font-normal text-gray-700 dark:text-gray-400">School Year: {(item.schoolYear)}</p>
-//                 </div>
-//               </Link>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
 return (
   <div>
     <SearchBar data={data} onSearch={handleSearch} />
-    <div className='grid grid-cols-3 gap-4 m-6'>
+    <article className='grid grid-cols-3 gap-4 m-6'>
       {searchResults.map(item => (
         <div key={item.id} >
           <Link href={item.file} key={item.id} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" target="_blank" rel="noreferrer">
@@ -59,15 +30,15 @@ return (
               height={200}
             />
             <div className="flex flex-col justify-between p-4 leading-normal">
-              <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
+              <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
               <p className="font-medium text-gray-700 dark:text-gray-400">Researchers: {item.author}</p>
               <p className="font-normal text-gray-700 dark:text-gray-400">Category: {item.category}</p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">School Year: {item.schoolYear}</p>
+              <p className="font-light text-gray-700 dark:text-gray-400">School Year: {item.schoolYear}</p>
             </div>
           </Link>
         </div>
       ))}
-    </div>
+    </article>
   </div>
 )}
 
