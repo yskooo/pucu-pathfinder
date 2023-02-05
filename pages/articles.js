50d4f6a90523f62945c93834373f935066b59f0f
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-05 17:24:21 +0800

    chore: add new component

--- pages/articles.js
+++ pages/articles.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import ArticleNav from '../components/search-articles/ArticleNav.jsx'
+import ArticleDesc from '../components/search-articles/ArticleDesc.jsx';
 import ResearchCard from '../components/search-articles/ResearchCard'
 
 const Search = () => {
@@ -8,12 +10,16 @@ const Search = () => {
         <title>Pathfinder | Articles</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/pucu.png" />
+        <link rel="icon" href="/pathfinder.png" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/flowbite.min.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/flowbite.min.js" defer/>
         <script src="../path/to/flowbite/dist/flowbite.min.js" defer/>
       </Head>
+      <nav classname='my-'>
+          <ArticleNav/>
+      </nav>
      <main className='contain min-h-screen'>
+          <ArticleDesc/>
            <ResearchCard/>
       </main>
     </>
