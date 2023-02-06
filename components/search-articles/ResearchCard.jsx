70cb98ee146176de99aef2428bbc000e26487347
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-06 20:26:22 +0800

    chore: enhance ResearchCard component

--- components/search-articles/ResearchCard.jsx
+++ components/search-articles/ResearchCard.jsx
@@ -11,17 +11,17 @@ const ResearchCard = () => {
     const filteredData = data.filter(item => {
       return item.title.toLowerCase().includes(searchTerm.toLowerCase())
     });
-
+    
     setSearchResults(filteredData);
   }
 
 return (
   <div>
-    <SearchBar data={data} onSearch={handleSearch} />
+    <SearchBar data={data} onSearch={handleSearch}/>
     <article className='grid grid-cols-3 gap-4 m-6'>
       {searchResults.map(item => (
         <div key={item.id} >
-          <Link href={item.file} key={item.id} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" target="_blank" rel="noreferrer">
+          <Link href={item.file} key={item.id} className="flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" target="_blank" rel="noreferrer">
             <Image
               className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" 
               src={item.image}
