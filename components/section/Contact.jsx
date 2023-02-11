56ea5274acfa33e3e1ab2768954ea24481fe82e7
Pam Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-11 23:10:27 +0800

    Update Contact.jsx

--- components/section/Contact.jsx
+++ components/section/Contact.jsx
@@ -7,7 +7,9 @@ const Contact = () => {
         <div className="mx-auto max-w-screen-sm text-center">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Get in Touch</h2>
             <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Do you have a feature request or you want to feature your research study in this platform?</p>
-            <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Send Us an Email</a>
+            <a href="#" className="text-white bg-yellow-300 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+              Send Us an Email
+            </a>
         </div>
     </div>
 </section>
