f356e105577dbf779b1e8b80ca2c9d028d5fcf21
Pam Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-11 23:09:34 +0800

    experiments

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,18 +3,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 const Banner = () => {
   return (
-    <section class="bg-[url('/banner.svg')] max-w-full h-screen bg-cover bg-center m-auto pt-20" id='home'>
-    <div class="py-8 px-4 mx-auto max-w-screen-2xl text-end lg:py-16 lg:px-12">
-        <h1 class="mb-4 mx-auto text-4xl font-extrabold tracking-tight leading-none text-yellow-300 md:text-5xl lg:text-6xl max-w-screen-lg">
+    <section class="bg-[url('/img/upangbg.svg')] w-full min-h-screen bg-cover bg-center m-auto pt-20" id='home'>
+    <div class="py-8 px-4 mx-auto max-w-screen-2xl text-center lg:py-16 lg:px-12">
+    {/* <Image
+            src='/img/pucu-white.svg'
+            className='w-1/2'
+            width={50}
+            height={50}
+        /> */}
+        <h1 class="mb-4 mx-auto text-5xl font-extrabold tracking-tight leading-none text-yellow-300 md:text-5xl lg:text-7xl max-w-screen-lg">
             Navigate the world of search
         </h1>
         <p class="mb-8 text-lg text-center font-light text-white lg:text-xl sm:px-16 xl:px-48">
           Find the study that suits your own in a vast selection waiting for you!
         </p>
         <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-emerald-700 hover:bg-primary-500 focus:ring-4 focus:ring-primary-500">
+            {/* <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-yellow-300 hover:bg-primary-500 focus:ring-4 focus:ring-primary-500">
                 Learn more
-            </a>
+            </a> */}
             <Link href="/articles" passHref target="_blank" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 focus:ring-4 focus:ring-gray-100">
                 Search articles now
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-2 w-6 h-6">
