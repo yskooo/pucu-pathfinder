d737cdca2c18596bdc1ce240692498230cfe721d
Pam Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-11 23:10:24 +0800

    Update Header.jsx

--- components/section/Header.jsx
+++ components/section/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from "next/image"
 
 const Header = () => {
   return (
-    <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white px-2 sm:px-4 py-2.5 w-full left-0 border-b border-gray-200">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
   <a href="" className="flex items-center">
     <h1 className='text-lg font-extrabold tracking-tight leading-none text-gray-800 md:text-xl lg:text-xl dark:text-white'>
-      PATHFINDER
+      <Image
+      src='img/pucupathlogo.svg'
+      className='w-5/6'
+      width={100}
+      height={100}
+      alt='logo'
+      />
     </h1>
   </a>
   <div className="flex md:order-2">
     <Link href='/articles'>
-      <button type="button" className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
+      <button type="button" className="text-white hover:bg-teal-800 bg-emerald-500 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
           <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clip-rule="evenodd" />
         </svg>
@@ -24,9 +31,9 @@ const Header = () => {
       </button>
    </div>
     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-      <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#home" className="block py-2 pl-3 pr-4 text-white bg-teal-700 rounded md:bg-transparent md:text-teal-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+          <a href="#home" className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-teal-700 md:p-0 dark:text-white" aria-current="page">Home</a>
         </li>
         <li>
           <a href="#about" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
