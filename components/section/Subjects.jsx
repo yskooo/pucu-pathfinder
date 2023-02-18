f08b87802e80c165b84f5654af88063d814d285d
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-18 21:31:56 +0800

    chore: add subjects cards components

---
+++ components/section/Subjects.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const Subjects = () => {
+  return (
+    <div>
+<section class="bg-white dark:bg-green-700">
+    <div class="container px-6 py-10 mx-auto animate-pulse">
+        <h1 className="text-2xl font-bold flex items-center justify-center">Subjects</h1>
+
+        <p class="dark:bg-green-700 text-center">Discover the subjects available here so you`&apos`re not mahihirapan on your research</p>
+
+        <div class="grid grid-cols-3 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+            <div class="w-full ">
+                {/* <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div> */}
+                <h1>APP 004</h1>
+                <p>Entrepreneurship</p>
+                {/* <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700">ABM</h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700">BLABLABLA</p> */}
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div class="w-full ">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+        </div>
+        <div class="flex justify-center items-center">
+                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                
+                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+    </div>
+</section>
+    </div>
+  )
+}
+
+export default Subjects
